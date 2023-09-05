10a7dd4a08724041cdbe1438bff0f9f6bba1e424
jefersoncaye <jefercaye@yahoo.com.br>
2023-09-05 15:42:39 -0300

    entrada usuario

---
+++ entradas_usuario.py
@@ -0,0 +1,8 @@
+nome = input('Digite seu nome: ')
+
+print(f'Olá {nome}')
+
+idade = int(input('Digite sua Idade: '))
+peso = float(input('Digite seu peso: '))
+
+print(f'Você nasceu no ano {2023 - idade}, seu peso é: {peso:.2f}')
