af6ad41401a06d6c8e489e2926925cddf809b241
jefersoncaye <jefercaye@yahoo.com.br>
2023-09-12 16:46:52 -0300

    while

---
+++ while_loop.py
@@ -0,0 +1,29 @@
+"""nome = input('Nome: ')
+
+while nome != 'Jade':
+    nome = input('Nome: ')
+    print('Estou no Loop')
+print('Sai do Loop')
+"""
+'''
+# Loop infinito
+while True:
+    print('Estou no Loop')'''
+
+# Break
+"""i = 1
+while i <= 6:
+    print(i)
+    if i == 4:
+        break
+    i += 1
+"""
+
+# else
+i = 1
+
+while i <= 6:
+    print(i)
+    i += 1
+else:
+    print('i é maior que 6')
